d4779708b591a55c91712990aed8cf419d1cad64
hiroyone <hirokiyonetani@gmail.com>
2021-04-19 18:35:39 +0900

    Add JSDoc and stubs for webhook file

--- lib/webhook.ts
+++ lib/webhook.ts
@@ -1,8 +1,10 @@
-// func redirectPolicyFunc(req gorequest.Request, via []gorequest.Request) error {
-// 	return fmt.Errorf("Incorrect token (redirection)")
-// }
-
-// func send(webhookUrl string, proxy string, payload Payload) []error {
+/**
+ * Post the message to Slack channel by webhook
+ * @param {string} webhookURL - Slack webhook URL
+ * @param {JSON} payload - the body message to be sent 
+ * @return {boolean} True if the post request is successful 
+ */
+function sendToSlack(webhookURL string, payload: JSON): boolean {
 // 	request := gorequest.New().Proxy(proxy)
 // 	resp, _, err := request.
 // 		Post(webhookUrl).
@@ -16,6 +18,5 @@
 // 	if resp.StatusCode >= 400 {
 // 		return []error{fmt.Errorf("Error sending msg. Status: %v", resp.Status)}
 // 	}
-
-// 	return nil
-// }
+	return true
+}
