c7c9720694d52c094c566f85ce08e66f16ccdbf9
hiroyone <hirokiyonetani@gmail.com>
2021-04-21 08:11:22 +0900

    Remove comment

--- utils/dates.ts
+++ utils/dates.ts
@@ -20,8 +20,6 @@ function nowToday(): string {
  */
 function checkWkday(dateTime: string): boolean {
   // default
-  x = false;
-  // if now.Weekday() >= 1 && now.Weekday() <= 5 {
   let weekdayFlag = false;
   let dateObj = new Date(dateTime);
   if (dateObj.getDay() >= 1 && dateObj.getDay() <= 5) {
