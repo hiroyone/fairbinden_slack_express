c1f4e8a46cdf99f92f83339ac0df744d6390441c
hiroyone <hirokiyonetani@gmail.com>
2021-04-21 10:57:37 +0900

    Comment out a short testing script

--- utils/dates.ts
+++ utils/dates.ts
@@ -51,9 +51,9 @@ function getJapaneseDate(dateTime: Date): string {
   return dateJpn;
 }
 
-let dateTime = nowToday();
-let dateFlag = checkWkday(dateTime);
-let dateJpn = getJapaneseDate(dateTime);
+// let dateTime = nowToday();
+// let dateFlag = checkWkday(dateTime);
+// let dateJpn = getJapaneseDate(dateTime);
 
-console.log(dateFlag);
-console.log(dateJpn);
+// console.log(dateFlag);
+// console.log(dateJpn);
