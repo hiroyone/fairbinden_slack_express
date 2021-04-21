2e2739a993f57977a5f75395eab0bb36c099238b
hiroyone <hirokiyonetani@gmail.com>
2021-04-21 10:57:13 +0900

    Convert from string to Date

--- utils/dates.ts
+++ utils/dates.ts
@@ -3,26 +3,24 @@
  * @example
  * // returns "4/19/2021, 8:53:49 AM"
  * nowToday();
- * @return {string} the current datime in Japanese Standard Time
+ * @return {Date} the current datime in Japanese Standard Time
  */
-function nowToday(): string {
+function nowToday(): Date {
   let now = new Date();
-  let nowISO = now.toISOString();
   // Info.Println("The current time is: ", jpn_now.String())
-  return nowISO;
+  return now;
 }
 
 /**
  * Returns if it is a weekday or not
- * @param {string} dateTime
+ * @param {Date} dateTime
  * @return {boolean} true if it is weekday, else not
  */
-function checkWkday(dateTime: string): boolean {
+function checkWkday(dateTime: Date): boolean {
   // default
   let weekdayFlag = false;
   // Create a date object
-  let dateObj = new Date(dateTime);
-  if (dateObj.getDay() >= 1 && dateObj.getDay() <= 5) {
+  if (dateTime.getDay() >= 1 && dateTime.getDay() <= 5) {
     weekdayFlag = true;
     // Info.Println("It is a Weekday today");
   } else {
@@ -35,21 +33,19 @@ function checkWkday(dateTime: string): boolean {
  * Returns today's date in Japanese language
  * @example
  * // returns "2021年04月01日(木)"
- * @param {string} dateTime
+ * @param {Date} dateTime
  * @return {string} today's date in Japanese language
  */
-function getJapaneseDate(dateTime: string): string {
+function getJapaneseDate(dateTime: Date): string {
   // Create a date object for convenience
-  let dateObj = new Date(dateTime);
   let daysJpn: string[];
   daysJpn = ["日", "月", "火", "水", "木", "金", "土"];
-  let dayJpn = daysJpn[dateObj.getDay()]; // => 木
+  let dayJpn = daysJpn[dateTime.getDay()]; // => 木
 
-  // // date = '{}月{}日({})'.format(now.month,now.day, yobi)
   // // logger.info("Today's date is {}".format(date))
-  let year = dateObj.getFullYear();
-  let month = dateObj.getMonth();
-  let day = dateObj.getDay();
+  let year = dateTime.getFullYear();
+  let month = dateTime.getMonth();
+  let day = dateTime.getDay();
   let dateJpn = `${year}年${month}月${day}日(${dayJpn})`;
   // fmt.Println(date)
   return dateJpn;
