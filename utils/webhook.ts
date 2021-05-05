2a3e740c25c5b6ccf8ffff172200eada66ae0288
hiroyone <hirokiyonetani@gmail.com>
2021-05-05 15:31:35 +0900

    Format code by prettier

--- utils/webhook.ts
+++ utils/webhook.ts
@@ -1,23 +1,23 @@
 /**
  * Post the message to Slack channel by webhook
  * @param {string} webhookURL - Slack webhook URL
- * @param {JSON} payload - the body message to be sent 
+ * @param {JSON} payload - the body message to be sent
  * @method POST
- * @return {boolean} True if the post request is successful 
+ * @return {boolean} True if the post request is successful
  */
 function sendToSlack(webhookURL: string, payload: JSON): boolean {
-// 	request := gorequest.New().Proxy(proxy)
-// 	resp, _, err := request.
-// 		Post(webhookUrl).
-// 		RedirectPolicy(redirectPolicyFunc).
-// 		Send(payload).
-// 		End()
+  // 	request := gorequest.New().Proxy(proxy)
+  // 	resp, _, err := request.
+  // 		Post(webhookUrl).
+  // 		RedirectPolicy(redirectPolicyFunc).
+  // 		Send(payload).
+  // 		End()
 
-// 	if err != nil {
-// 		return err
-// 	}
-// 	if resp.StatusCode >= 400 {
-// 		return []error{fmt.Errorf("Error sending msg. Status: %v", resp.Status)}
-// 	}
-	return true
+  // 	if err != nil {
+  // 		return err
+  // 	}
+  // 	if resp.StatusCode >= 400 {
+  // 		return []error{fmt.Errorf("Error sending msg. Status: %v", resp.Status)}
+  // 	}
+  return true;
 }
