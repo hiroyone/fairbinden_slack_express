3738ebdb6f1348c889cb02447592f39198068578
hiroyone <hirokiyonetani@gmail.com>
2021-05-05 13:25:40 +0900

    Fix the import&Convert from var to let or const

--- app.ts
+++ app.ts
@@ -1,12 +1,11 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError = require("http-errors");
+import express = require("express");
+import path = require("path");
+import cookieParser = require("cookie-parser");
+import logger = require("morgan");
+import { fairbnRouter } from "./routes/fairbindenRoute";
 
-var fairbnRouter = require("./routes/fairbindenRoute");
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- routes/fairbindenRoute.ts
+++ routes/fairbindenRoute.ts
@@ -3,12 +3,10 @@ import { MiddlewareFn } from "../interfaces/middlewareInterface";
 import express = require("express");
 import { sendFairbindenLunchMenuToSlack } from "../controllers/fairbindenController";
 
-const router = express.Router();
+export const fairbnRouter = express.Router();
 
 /* Post a fairbinden request. */
-router.post("/", <MiddlewareFn>function (req, res, next) {
+fairbnRouter.post("/", <MiddlewareFn>function (req, res, next) {
   sendFairbindenLunchMenuToSlack(req, res, next);
   res.send("success");
 });
-
-module.exports = router;
