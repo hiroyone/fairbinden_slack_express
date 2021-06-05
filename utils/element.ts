732fb72d827336a92e20b23ddca740880ecc3769
hiroyone <hirokiyonetani@gmail.com>
2021-06-05 22:43:49 +0900

    Split into two files

---
+++ utils/element.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+/**
+ * Returns the element from a page by given URL and selectors
+ */
+export async function getElementBySelectors(
+  pageURL: URL,
+  selectors: string
+): Promise<Element | void> {
+  try {
+    const response = await axios.get(pageURL.href);
+    const pageHTML = new JSDOM(response.data);
+
+    const elementBySelectors = pageHTML.window.document.querySelector(
+      selectors
+    );
+
+    if (elementBySelectors === null) {
+      throw new Error("The element does not exists for the given selectors!");
+    } else {
+      // Info.Println("Element is: ", elementBySelectors)
+      return elementBySelectors;
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
