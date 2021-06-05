533fdc7592b7a8e14ccd3f2b58e81f1c28d64be4
hiroyone <hirokiyonetani@gmail.com>
2021-06-05 10:23:21 +0900

    Implement getTitle method

--- utils/postParsers.ts
+++ utils/postParsers.ts
@@ -1,39 +1,36 @@
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
 /**
  * Returns the title in the URL page
- * @param {string} pageURL
+ * @param {URL} pageURL
+ * @example new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/")
+ * returns "豚肉のズッキーニ巻きフライ"
+ * getTitle(new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/%e8%b1%9a%e8%82%89%e3%81%ae%e3%82%ba%e3%83%83%e3%82%ad%e3%83%bc%e3%83%8b%e5%b7%bb%e3%81%8d%e3%83%95%e3%83%a9%e3%82%a4-6/"),"#single_post > h2")
  * @return {string} the title from the URL
  */
-function getTitle(pageURL: string): string {
-  // url, _ := url.Parse(dayMenuURL)
-  // domain := url.Host
-  // Info.Println("Allowed Domain:", domain)
-  // var title string
+export async function getTitle(
+  pageURL: URL,
+  selectors: string
+): Promise<string | void> {
+  try {
+    const response = await axios.get(pageURL.href);
+    const dailyMenuPageHtml = new JSDOM(response.data);
 
-  // c := colly.NewCollector(
-  // 	// Restrict crawling to specific domains
-  // 	colly.AllowedDomains(domain),
-  // 	// Allow visiting the same page multiple times
-  // 	colly.AllowURLRevisit(),
-  // 	// Allow crawling to be done in parallel / async
-  // 	colly.Async(false),
-  // )
-  // c.Limit(&colly.LimitRule{
-  // 	// Filter domains affected by this rule
-  // 	DomainGlob: domain + "/*",
-  // 	// Set a delay between requests to these domains
-  // 	Delay: 1 * time.Second,
-  // 	// Add an additional random delay
-  // 	RandomDelay: 1 * time.Second,
-  // })
+    const titleElement = dailyMenuPageHtml.window.document.querySelector(
+      selectors
+    );
 
-  // c.OnHTML(".post_title", func(e *colly.HTMLElement) {
-  // 	title = e.Text
-  // })
-  // c.Visit(dayMenuURL)
-
-  // Info.Println("Title is: ", title)
-  // return &title
-  return "豚肉のズッキーニ巻きフライ";
+    if (titleElement === null) {
+      throw new Error("The element does not exists for the given selectors!");
+    } else {
+      // Info.Println("Title is: ", title)
+      // return "豚肉のズッキーニ巻きフライ";
+      return titleElement.innerHTML;
+    }
+  } catch (err) {
+    alert(err);
+  }
 }
 
 /**
