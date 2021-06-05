92d3cbd85cd21caab47251698fa98d827039e7fb
hiroyone <hirokiyonetani@gmail.com>
2021-06-05 21:30:42 +0900

    Remove JSDoc for parse util file

--- utils/postParsers.ts
+++ utils/postParsers.ts
@@ -6,9 +6,6 @@ import { JSDOM } from "jsdom";
  * @example
  * // returns new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/%e8%b1%9a%e8%82%89%e3%81%ae%e3%82%ba%e3%83%83%e3%82%ad%e3%83%bc%e3%83%8b%e5%b7%bb%e3%81%8d%e3%83%95%e3%83%a9%e3%82%a4-6/")
  * getDailyMenuURL(new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/"), "#archive_post_list > li > div > h3 > a")
- * @param {URL} dailyURL
- * @param {string} selectors
- * @return {URL} the menu URL for the specified date
  */
 export async function getDailyMenuURL(
   dailyURL: URL,
@@ -44,11 +41,9 @@ export async function getDailyMenuURL(
 
 /**
  * Returns the title in the URL page
- * @param {URL} pageURL
  * @example
  * // returns "豚肉のズッキーニ巻きフライ"
  * getTitle(new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/%e8%b1%9a%e8%82%89%e3%81%ae%e3%82%ba%e3%83%83%e3%82%ad%e3%83%bc%e3%83%8b%e5%b7%bb%e3%81%8d%e3%83%95%e3%83%a9%e3%82%a4-6/"),"#single_post > h2")
- * @return {string} the title from the URL
  */
 export async function getTitle(
   pageURL: URL,
@@ -75,11 +70,9 @@ export async function getTitle(
 
 /**
  * Returns the main texts in the URL page
- * @param {string} pageURL
  * @example
  * // return "群馬県の下仁田ミートを使って ...";
  * getMainTexts(new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/%e8%b1%9a%e8%82%89%e3%81%ae%e3%82%ba%e3%83%83%e3%82%ad%e3%83%bc%e3%83%8b%e5%b7%bb%e3%81%8d%e3%83%95%e3%83%a9%e3%82%a4-6/"),"#single_post > div.post_content.clearfix")
- * @return {string} the main texts from URL
  */
 export async function getMainText(
   pageURL: URL,
@@ -105,11 +98,9 @@ export async function getMainText(
 
 /**
  * Returns the main image URL in the URL page
- * @param {string} pageURL
  * @example
  * // return "http://xn--jvrr89ebqs6yg.tokyo/wp-content/uploads/2021/04/IMG_0408-2048x1536.jpg";
  * getMainTexts(new URL("https://xn--jvrr89ebqs6yg.tokyo/2021/04/19/%e8%b1%9a%e8%82%89%e3%81%ae%e3%82%ba%e3%83%83%e3%82%ad%e3%83%bc%e3%83%8b%e5%b7%bb%e3%81%8d%e3%83%95%e3%83%a9%e3%82%a4-6/"),"#single_post > div.post_image > img")
- * @return {string} the main image URL in the URL page
  */
 export async function getImageURL(
   pageURL: URL,
