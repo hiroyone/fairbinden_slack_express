afb0cc63019817fcc49ae249aa6a09bf0e3f199b
hiroyone <hirokiyonetani@gmail.com>
2021-06-23 01:00:28 +0900

    Add try catch statement for lunchInfo builder

--- builder/lunchInfo.ts
+++ builder/lunchInfo.ts
@@ -12,45 +12,48 @@ export async function getLunchInfo(
   dateTime: Date,
   dateFlag: boolean,
   lunchWebsite: Website
-): Promise<lunchInfo> {
-  const dailyURL = createDayURL(
-    dateTime,
-    lunchWebsite.protocol as Protocol,
-    lunchWebsite.host
-  );
-
-  let dailyMenuURL: void | URL | null;
-  if (dateFlag) {
-    dailyMenuURL = await getDayMenuURL(
-      dailyURL,
-      "#archive_post_list > li > div > h3 > a"
+): Promise<lunchInfo | undefined> {
+  try {
+    const dailyURL = createDayURL(
+      dateTime,
+      lunchWebsite.protocol as Protocol,
+      lunchWebsite.host
     );
-  } else {
-    throw "Today is not a weekday.";
+
+    let dailyMenuURL: void | URL | null;
+    if (dateFlag) {
+      dailyMenuURL = await getDayMenuURL(
+        dailyURL,
+        "#archive_post_list > li > div > h3 > a"
+      );
+    } else {
+      throw "Today is not a weekday.";
+    }
+
+    // If daily menu URL is found, get article contents
+    let menuMainText, menuTitle, menuImageURL;
+    if (dailyMenuURL) {
+      menuTitle = await getTitle(dailyMenuURL, "#single_post > h2");
+      menuMainText = await getMainText(
+        dailyMenuURL,
+        "#single_post > div.post_content.clearfix"
+      );
+      menuImageURL = await getImageURL(
+        dailyMenuURL,
+        "#single_post > div.post_image > img"
+      );
+    } else {
+      throw "Daily Menu URL does not exists";
+    }
+
+    const fairbindenLunchInfo = {
+      dailyMenuURL: dailyMenuURL,
+      menuMainText: menuMainText,
+      menuTitle: menuTitle,
+      menuImageURL: menuImageURL,
+    };
+    return fairbindenLunchInfo;
+  } catch (error) {
+    console.error(error);
   }
-
-  // If daily menu URL is found, get article contents
-  let menuMainText, menuTitle, menuImageURL;
-  if (dailyMenuURL) {
-    menuTitle = await getTitle(dailyMenuURL, "#single_post > h2");
-    menuMainText = await getMainText(
-      dailyMenuURL,
-      "#single_post > div.post_content.clearfix"
-    );
-    menuImageURL = await getImageURL(
-      dailyMenuURL,
-      "#single_post > div.post_image > img"
-    );
-  } else {
-    throw "Daily Menu URL does not exists";
-  }
-
-  const fairbindenLunchInfo = {
-    dailyMenuURL: dailyMenuURL,
-    menuMainText: menuMainText,
-    menuTitle: menuTitle,
-    menuImageURL: menuImageURL,
-  };
-
-  return fairbindenLunchInfo;
 }
